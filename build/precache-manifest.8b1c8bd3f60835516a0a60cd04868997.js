6de3d0d6a635c54395e801635163ed34e2282e4f
shaokuangkuang <314462224@qq.com>
2019-11-07 11:11:57 +0800

    first

---
+++ build/precache-manifest.8b1c8bd3f60835516a0a60cd04868997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf217b849d097ba7458facd540affe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdc333b33d522245e556",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "74dd57687751c58558e0",
+    "url": "/static/css/main.7b8f4d59.chunk.css"
+  },
+  {
+    "revision": "fdc333b33d522245e556",
+    "url": "/static/js/2.8a966064.chunk.js"
+  },
+  {
+    "revision": "74dd57687751c58558e0",
+    "url": "/static/js/main.a038da83.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
